3c976ceaa8be8829820ece2c56d4a62771ee3690
Štěpán Tomsa <115143064+Tomsatomsic@users.noreply.github.com>
2023-03-13 10:30:32 +0100

    Add files via upload

---
+++ res/js/main.js
@@ -0,0 +1,10 @@
+const play = document.getElementById("play")
+const pokelogo = document.getElementById("pokelogo")
+const lobby = document.getElementById("lobby")
+
+
+function start() {
+    pokelogo.style.display = "none";
+    play.style.display = "none";
+    lobby.style.display = "flex";
+}
