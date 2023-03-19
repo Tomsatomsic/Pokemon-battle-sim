561ea3ac05ec46b4acde456f5f94daff709ae5c4
Štěpán Tomsa <115143064+Tomsatomsic@users.noreply.github.com>
2023-03-19 20:15:55 +0100

    Update main.js

--- res/js/main.js
+++ res/js/main.js
@@ -1,10 +1,23 @@
 const play = document.getElementById("play")
 const pokelogo = document.getElementById("pokelogo")
 const lobby = document.getElementById("lobby")
+const pokemon = document.getElementById("pokemon")
+const battle = document.getElementById("battle")
+const menu = document.getElementById("menu")
+const pokemans = document.getElementById("pokemans")
+
+
 
 
 function start() {
     pokelogo.style.display = "none";
     play.style.display = "none";
     lobby.style.display = "flex";
+    menu.style.display="flex";
 }
+
+function pokemon(){
+pokemans.style.display="flex";
+}
+
+
